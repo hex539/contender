699bac1c8d1d8fffc6941365ca4fedfac987501a
Robin Lee <rl411@bath.ac.uk>
2014-04-20 12:04:34 +0000

    Update debug script

--- judge/script/debug.sh
+++ judge/script/debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo -n 'stopping judge... ' >&2
-(killall 'judge_server.pl' 2> /dev/null && echo '[OK]' >& 2) || echo '[FAILED]' >&2
+(kill `ps axu | grep [j]udge_server.pl | awk '{print $2'}` 2> /dev/null && echo '[OK]' >& 2) || echo '[FAILED]' >&2
 
 echo    'starting judge... ' >&2
-nohup ./judge_server.pl -d --keeperr 2>>../error.log >/dev/null
+nohup ./script/judge_server.pl -d --keeperr 2>>./error.log >/dev/null
 echo 'finished.'
