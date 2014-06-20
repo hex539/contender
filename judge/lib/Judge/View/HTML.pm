543131026efc99f5455416e23844d23a6ae21882
Robin Lee <rl411@bath.ac.uk>
2014-06-21 00:37:34 +0100

    Remove boilerplate perldoc from HTML.pm

--- judge/lib/Judge/View/HTML.pm
+++ judge/lib/Judge/View/HTML.pm
@@ -5,19 +5,7 @@ use warnings;
 
 use base 'Catalyst::View::Xslate';
 
-__PACKAGE__->config(
-    template_extension => '.tx',
-);
+__PACKAGE__->config(template_extension => '.tx');
 
 1;
-
-=head1 NAME
-
-Judge::View::HTML - Xslate View for Judge
-
-=head1 DESCRIPTION
-
-Xslate View for Judge.
-
-=cut
 
