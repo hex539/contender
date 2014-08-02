3666b737b22f68181158c4fb6c7414f0d5573a2f
Robin Lee <rl411@bath.ac.uk>
2014-08-02 19:23:46 +0000

    Convert UI to polymer look
    Brings design in line with Google's Material Design guidelines. There
    is still some work to do on this but for the moment it looks
    convincing enough.
    
    Also fixes some UI bugs in the main code

--- judge/lib/Settings.pm
+++ judge/lib/Settings.pm
@@ -1,16 +1,12 @@
-#!/usr/bin/perl
-
-package Config;
+package Settings;
 
 use strict;
 use warnings;
 use feature 'state';
-use Exporter;
 
+use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(judgeroot);
-
-use Schema;
 
 sub judgeroot {'/home/judge/data'}
 
