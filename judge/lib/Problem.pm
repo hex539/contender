16dc82cac88b6987ed51b8599d840216d2f55cb9
Robin Lee <rl411@bath.ac.uk>
2014-08-02 19:28:53 +0000

    Add 'dir' and 'models' routines to Problems API
    Makes Problems-related code a bit easier to write without dropping to
    any hacks like guessing problem statement filenames

--- judge/lib/Problem.pm
+++ judge/lib/Problem.pm
@@ -1,28 +1,32 @@
 package Problem;
 
-use Exporter;
-our @EXPORT_OK = qw(tests);
-
-use File::Slurp;
-use HTML::Defang;
-use Text::Markdown qw(markdown);
-use DateTime::Format::MySQL;
-use HTML::Entities;
-use File::Basename;
-use File::Spec::Functions;
+use strict;
+use warnings;
 use feature 'state';
 
-my $judgeroot = '/home/judge/data';
+use Exporter;
+our @EXPORT_OK = qw(dir tests models);
+
+use File::Slurp;
+use File::Spec::Functions;
+use HTML::Defang;
+use Text::Markdown qw(markdown);
+
+use Settings qw(judgeroot);
+
+sub dir {
+  my ($self, @subdirs) = @_;
+
+  return catdir(judgeroot, 'problems', $self->id, @subdirs);
+}
 
 sub tests {
   my $self = shift;
   my %types = map {$_ => 1} @_;
 
-  use File::Spec::Functions;
-  my $testdir = catdir($judgeroot, 'problems', $self->id, 'tests');
   my @results = ();
 
-  opendir ((my $dir), $testdir);
+  opendir ((my $dir), dir($self, 'tests'));
   while (readdir $dir) {
     if ((my $name = $_) =~ /([a-zA-Z]+)(\d+)\.in/) {
       my $type = $1;
@@ -33,14 +37,37 @@ sub tests {
       push @results, {
         type    => $type,
         id      => int($num),
-        input   => scalar read_file(catfile($testdir, "$type$num.in")),
-        output  => scalar read_file(catfile($testdir, "$type$num.out")),
+        input   => scalar read_file(catfile(dir($self, 'tests'), "$type$num.in")),
+        output  => scalar read_file(catfile(dir($self, 'tests'), "$type$num.out")),
       };
     }
   }
   closedir $dir;
 
-  return [sort {($a->{type} cmp $b->{type}) || ($a->{id} <=> $b->{id})} @results];
+  return [sort {($a->{type} cmp $b->{type})
+             || ($a->{id}   <=> $b->{id})} @results];
+}
+
+sub models {
+  my $self = shift;
+  my %filters = map {$_ => 1} @_;
+
+  my @results = ();
+
+  opendir ((my $dir), dir($self, 'models'));
+  while (readdir $dir) {
+    if ((my $name = $_) =~ /[-a-zA-Z_]+\.(?:cc|py)/) {
+      push @results, {
+        name    => $name,
+        lang    => 'C++',
+        source  => scalar read_file(catfile(dir($self, 'models'), $name)),
+      };
+    }
+  }
+  closedir $dir;
+
+  return [sort {$a->{lang} cmp $b->{lang}
+             || $a->{name} cmp $b->{name}} @results];
 }
 
 1;
