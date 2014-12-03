6fdfbc1b03ed86a72c159fe768df705127884516
Robin Lee <rl411@bath.ac.uk>
2014-12-03 14:50:14 +0000

    Writeup page

---
+++ judge/lib/Judge/Controller/writeup.pm
@@ -0,0 +1,46 @@
+package Judge::Controller::writeup;
+use Moose;
+use namespace::autoclean;
+
+use File::Slurp;
+use Database;
+use User;
+use Problem;
+use HTML::Defang;
+use Text::Markdown qw(markdown);
+use File::Spec::Functions;
+use feature 'state';
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+my $judgeroot = '/home/judge/data';
+
+sub writeup
+  :Chained("/problem/problem")
+  :PathPart("writeup")
+  :Args(0) {
+
+  my ($self, $c, $id) = @_;
+
+  my $problem = $c->stash->{problem};
+  my $content = '';
+  eval {
+    $content = read_file(catfile($judgeroot, 'problems', $problem->id, 'writeup.markdown'));
+  };
+  eval {
+    use Text::Xslate qw(mark_raw);
+    $content = markdown $content;
+    $content = HTML::Defang->new(fix_mismatched_tags => 1)->defang($content);
+    $content = mark_raw($content);
+  };
+
+  $c->stash(
+    template => 'writeup.tx',
+    content => $content,
+    models => [],
+  );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
