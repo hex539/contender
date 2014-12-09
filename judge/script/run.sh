ffb9354e5fef95da10f845d8c23dc703822d2c95
Robin Lee <rl411@bath.ac.uk>
2014-12-09 19:34:06 +0000

    Save run.sh error logs to current directory
    The old behaviour was confusing. Why log to ../?

--- judge/script/run.sh
+++ judge/script/run.sh
@@ -4,5 +4,5 @@ echo -n 'stopping judge... ' >&2
 (killall 'perl-fcgi-pm [Judge]' 2> /dev/null && echo '[OK]' >& 2) || echo '[FAILED]' >&2
 
 echo    'starting judge... ' >&2
-./judge_fastcgi.pl --listen 127.0.0.1:30115 --keeperr 2>>../error.log >/dev/null
+./judge_fastcgi.pl --listen 127.0.0.1:30115 --keeperr 2>>./error.log >/dev/null
 echo 'finished.'
