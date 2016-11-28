458a5dc104e4bba157e5ff22f724401e3ef74f82
Robin Lee <robin@hex539.me>
2016-11-28 21:55:09 +0000

    Enable sign in for the root page
    (should have been done from the start, not very bright)

--- judge/lib/Judge/Controller/Root.pm
+++ judge/lib/Judge/Controller/Root.pm
@@ -2,17 +2,30 @@ package Judge::Controller::Root;
 use Moose;
 use namespace::autoclean;
 
+use strict;
+use warnings;
+use feature 'state';
+
 BEGIN { extends 'Catalyst::Controller' }
 __PACKAGE__->config(namespace => '');
 
 use Settings;
+use User;
 
 sub index :Path :Args(0) {
   my ($self, $c) = @_;
+
+  # Is our user signed in?
+  my $user = User::get($c);
+
+  $c->stash(
+    user => $user,
+  );
 }
 
 sub default :Path {
   my ($self, $c) = @_;
+
   $c->stash(
     template => '404.tx',
     title => 'Not Found',
