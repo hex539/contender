ab9babefa04d74a2aa42b0fe9bc40ecbc6a7eb04
Robin Lee <rglee32@gmail.com>
2017-01-24 00:07:47 +0000

    Re-enable the countdown script
    Now everything is using UNIX epoch timestamps all over the plae it's a
    lot easier to reason about what might / might not be going wrong -- and
    in fact over the course of refactoring everything to use UNIX
    timestamps, the original issues went away.
    
    Although at time of writing we in the UK are in the +0000 timezone,
    one of the biggest advantages of which is masking stupid issues in time
    calculations -- at least for half of the year.

--- judge/root/static/scripts/countdown.js
+++ judge/root/static/scripts/countdown.js
@@ -1,36 +1,37 @@
-$(function(){
-  /* Something wrong with this */
-  /* TODO: investigate after 14/10/13 */
+if (typeof(contest_start) !== 'undefined' && contest_start !== null) {
+  $(function(){
+    var tex = $('#time_elapsed');
 
-  var tex = $('#time_elapsed');
+    var ok = false;
+    var ival = null;
 
-  var ok = false;
-  var ival = null;
+    ival = window.setInterval(function() {
+      var now = new Date();
+      var diff = now.getTime() - contest_start.getTime();
+      var since = new Date(diff);
 
-  ival = window.setInterval(function() {
-    var now = new Date();
-    var diff = now.getTime() - contest_start.getTime();
-    var since = new Date(diff);
+      if (diff < contest_duration * 1000 + 1000) {
+        ok = true;
+      }
+      else {
+        window.clearInterval(ival);
 
-    if (diff < contest_duration * 1000 + 1000) {
-      ok = true;
-    }
-    else {
-      window.clearInterval(ival);
+        if (ok) {
+          location.reload();
+        }
+        return;
+      }
 
-//      if (ok) {
-//        location.reload();
-//      }
-      return;
-    }
+      var fmt = function(s) {
+        s = new String(s);
+        while (s.length < 2) s = '0' + s;
+        return s;
+      }
 
-    var fmt = function(s) {
-      s = new String(s);
-      while (s.length < 2) s = '0' + s;
-      return s;
-    }
+      tex.text(fmt(since.getUTCHours()) +
+         ':' + fmt(since.getUTCMinutes()) +
+         ':' + fmt(since.getUTCSeconds()));
 
-    tex.text(fmt(since.getUTCHours()) + ':' + fmt(since.getUTCMinutes()) + ':' + fmt(since.getUTCSeconds()));
-
-  }, 1000);
-});
+    }, 1000);
+  });
+}
